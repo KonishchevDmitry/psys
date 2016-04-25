669f4612d55bcedd693a01328673468bc615fa12
Mikhail Ushanov <MiUshanov@croc.ru>
2016-04-25 17:39:56 +0300

    Added psys.pipe module that represents UNIX pipe
    Signed-off-by: Mikhail Ushanov <MiUshanov@croc.ru>

---
+++ psys/pipe.py
@@ -0,0 +1,55 @@
+"""Provides a wrapper around a UNIX pipe."""
+
+import fcntl
+import logging
+import os
+
+from psys import eintr_retry
+
+
+LOG = logging.getLogger(__name__)
+"""Logger instance."""
+
+
+class Pipe(object):
+    """Represents a UNIX pipe."""
+
+    read = None
+    """Read FD."""
+
+    write = None
+    """Write FD."""
+
+    def __init__(self, nonblock=False):
+        self.read, self.write = os.pipe()
+
+        try:
+            if nonblock:
+                for fd in self.read, self.write:
+                    flags = eintr_retry(fcntl.fcntl)(fd, fcntl.F_GETFL)
+                    eintr_retry(fcntl.fcntl)(fd, fcntl.F_SETFL, flags | os.O_NONBLOCK)
+        except:
+            self.close()
+            raise
+
+    def __del__(self):
+        self.close()
+
+    def close(self, read=True, write=True):
+        """Closes the pipe."""
+
+        if read and self.read is not None:
+            try:
+                eintr_retry(os.close)(self.read)
+            except Exception as e:
+                LOG.error("Unable to close a pipe: %s.", e)
+            else:
+                self.read = None
+
+        if write and self.write is not None:
+            try:
+                eintr_retry(os.close)(self.write)
+            except Exception as e:
+                LOG.error("Unable to close a pipe: %s.", e)
+            else:
+                self.write = None
